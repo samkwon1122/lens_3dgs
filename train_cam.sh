a45f759fd9a54d09d3bd27fa343f8ef359685fbe
samkwon1122 <kwonhc1122@gm.gist.ac.kr>
2024-10-27 22:08:51 +0900

    gpu_ids and ip

--- train_cam.sh
+++ train_cam.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Usage: ./train_cam.sh GreatCourt/seq1
+# Usage: ./train_cam.sh <dataset>/<seq> <gpu_ids> <ip>
+# Usage: ./train_cam.sh GreatCourt/seq1 0,1,2,3 1
 
 TARGET_DIR="/data1/heungchan/CambridgeLandmarks/$1"
 INPUT_DIR="$TARGET_DIR/input"
@@ -9,6 +10,6 @@ mkdir -p "$INPUT_DIR"
 
 mv "$TARGET_DIR"/*.png "$INPUT_DIR"
 
-python convert.py -s $TARGET_DIR --resize
-python train.py -s $TARGET_DIR -r 1 -m output/$1
-python render.py -m output/$1
+CUDA_VISIBLE_DEVICES=$2 python convert.py -s $TARGET_DIR --resize
+CUDA_VISIBLE_DEVICES=$2 python train.py -s $TARGET_DIR -r 1 -m output/$1 --ip 127.0.0.$3
+CUDA_VISIBLE_DEVICES=$2 python render.py -m output/$1
